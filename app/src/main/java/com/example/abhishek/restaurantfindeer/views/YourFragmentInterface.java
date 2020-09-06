f30fb58e8b087c8bb5d8b95e82c7c07a6276f593
Abhishek Singh Adhikari <adhikariabhishek290@gmail.com>
2020-09-06 23:09:00 +0530

    Update YourFragmentInterface.java

--- app/src/main/java/com/example/abhishek/restaurantfindeer/views/YourFragmentInterface.java
+++ app/src/main/java/com/example/abhishek/restaurantfindeer/views/YourFragmentInterface.java
@@ -1,8 +1,5 @@
 package com.example.abhishek.restaurantfindeer.views;
 
-/**
- * Created by jitesh.upadhyay on 8/7/2017.
- */
 
 public interface YourFragmentInterface {
     public void fragmentBecameVisible(String query);
